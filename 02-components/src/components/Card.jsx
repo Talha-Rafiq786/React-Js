92767be7bb02a1d2ac9d6db4791e5e2d0b1a9820
Talha-Rafiq786 <talharafeeq597@email@domain.com>
2025-11-12 21:40:29 +0500

    Components complete

---
+++ 02-components/src/components/Card.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Card = () => {
+  return (
+    <div>
+        <div className='card'>
+        <h1>Talha Rafeeq</h1>
+        <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Nisi ipsam provident sit! Eaque, rem nihil.</p>
+      </div>
+    </div>
+  )
+}
+
+export default Card
